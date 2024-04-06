3fe2e7558be5da90b76e750d711f9f91d1b0e6dc
Ama <ama@clearbledesign.com>
2024-04-07 03:21:59 +0530

    Cors

--- ResumeCreator/Program.cs
+++ ResumeCreator/Program.cs
@@ -9,7 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<ResumeService>(); // Add TemplateProcessor as a transient service
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
